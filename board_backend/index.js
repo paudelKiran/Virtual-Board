3a88389a0c8cbe1fd71e80afb369b705d0fc28b8
paudel_kiran <kpkiran9999@gmail.com>
2024-08-06 23:01:29 +0545

    join and create room made workable

--- board_backend/index.js
+++ board_backend/index.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const { Server } = require("socket.io");
+const { createServer } = require("http");
 const app = express();
-const port = 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+const port = 9000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+io.on("connection", (socket) => {
+  // console.log("Ommm chin tabak dam dam");
+
+  //? data requested from create room
+  socket.on("createRoomData", (data) => {
+    const { roomId, userId, userName, host, meetingTitle, presenter } = data;
+    socket.join(roomId);
+    socket.emit("roomCreated", { success: true });
+    console.log("create triggered");
+  });
+
+  //? data requested from join room
+  socket.on("joinRoomData", (data) => {
+    const { roomId, userId, userName, host, presenter } = data;
+    socket.join(roomId);
+    socket.emit("roomJoined", { success: true });
+    console.log("join triggered");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
 });
